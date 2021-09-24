0f39208c03df74b8f36bfbf0cfb1c0b04639d2e8
handrianalandi <handrianalandi@users.noreply.github.com>
2021-09-24 09:09:11 +0700

    a

--- home.php
+++ home.php
@@ -134,6 +134,7 @@ if (isset($_GET['status'])) {
 				</div>
 			</div>
 		</div>
+		asdwdasd
 
 		<div class="modal fade" id="login-modal" tabindex="-1" role="dialog" aria-hidden="true">
 			<div class="modal-dialog modal-lg" role="document">
